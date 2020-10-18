7f91f027eca4d48600578d45c49daac8dd7bcf25
Vladimir Yakovlev <Vya@slb.com>
2020-10-17 19:45:58 -0500

    add telegraf and mdt subsriptions

---
+++ telegraf/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -m
+CONFIG_TEMPLATE="/telegraf.template.conf"
+CONFIG_FILE="/etc/telegraf/telegraf.conf"
+
+sed -e "s/\${HOST_NAME}/$HOST_NAME/" \
+ -e "s!\${INFLUXDB_HOST}!$INFLUXDB_HOST!" \
+ -e "s/\${INFLUXDB_PORT}/$INFLUXDB_PORT/" \
+ -e "s/\${DATABASE}/$DATABASE/" \
+ -e "s/\${GRPC_PORT}/$GRPC_PORT/" \
+ $CONFIG_TEMPLATE > $CONFIG_FILE
+
+echo "=> Starting Telegraf ..."
+
+exec telegraf -config /etc/telegraf/telegraf.conf
