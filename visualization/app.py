b77793f86d24d934b6f4b9b6dbea91ed5741ba0f
PMoorey <PMoorey@slb.com>
2020-10-18 15:22:58 -0500

    Updated Plotly Dash to use lab Influx

--- visualization/app.py
+++ visualization/app.py
@@ -6,13 +6,13 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-client = InfluxDBClient(host='influxdb', port=8086, username='root', password='root', database='routing_compliance')
+client = InfluxDBClient(host='163.185.202.37', port=8086, username='root', password='root', database='telegraf')
 q = 'SELECT * from routingCompliance'
 df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 
-routers = [{'label': 'All', 'value': '*'}]
-for v in df.source.unique():
-    routers.append({'label': v, 'value': v})
+# routers = [{'label': 'All', 'value': '*'}]
+# for v in df.source.unique():
+#     routers.append({'label': v, 'value': v})
 
 prefixes = [{'label': 'All', 'value': '*'}]
 for v in df.prefix.unique():
@@ -20,10 +20,8 @@ for v in df.prefix.unique():
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-fig_community_compliance = px.area(df, x="time", y="community_compliance", color="prefix")
-fig_as_origin_compliance = px.area(df, x="time", y="as_origin_compliance", color="prefix")
-fig_weight_compliance = px.area(df, x="time", y="weight_compliance", color="prefix")
-fig_med_compliance = px.area(df, x="time", y="med_compliance", color="prefix")
+fig_origin_compliance = px.area(df, x="time", y="origin_compliance", color="prefix")
+fig_as_path_compliance = px.area(df, x="time", y="as_path_compliance", color="prefix")
 
 app.layout = html.Div(children=[
     html.H1(children='BGP Routing Compliance Dashboard'),
@@ -31,12 +29,12 @@ app.layout = html.Div(children=[
     html.H3(children='''NANOG 80 Hackathon'''),
 
     html.Div([
-        html.Label('Select a router'),
-        dcc.Dropdown(
-            options=routers,
-            value='All',
-            multi=True
-        ),
+        # html.Label('Select a router'),
+        # dcc.Dropdown(
+        #     options=routers,
+        #     value='All',
+        #     multi=True
+        # ),
         html.Label('Select a prefix'),
         dcc.Dropdown(
             options=prefixes,
@@ -47,34 +45,18 @@ app.layout = html.Div(children=[
 
     html.Div([
             html.Div([
-                    html.H4('Community Value - Count of Non-Compliant Events'),
+                    html.H4('Origin - Count of Non-Compliant Events'),
                     dcc.Graph(
-                        id='fig_community_compliance',
-                        figure=fig_community_compliance
+                        id='fig_origin_compliance',
+                        figure=fig_origin_compliance
                     )
                 ]
             ),
             html.Div([
-                    html.H4('AS Origin Compliance - Count of Non-Compliant Events'),
+                    html.H4('AS Path Compliance - Count of Non-Compliant Events'),
                     dcc.Graph(
-                        id='fig_as_origin_compliance',
-                        figure=fig_as_origin_compliance
-                    )
-                ]
-            ),
-            html.Div([
-                    html.H4('Weight Compliance - Count of Non-Compliant Events'),
-                    dcc.Graph(
-                        id='fig_weight_compliance',
-                        figure=fig_weight_compliance
-                    )
-                ]
-            ),
-            html.Div([
-                    html.H4('MED Compliance - Count of Non-Compliant Events'),
-                    dcc.Graph(
-                        id='fig_med_compliance',
-                        figure=fig_med_compliance
+                        id='fig_as_path_compliance',
+                        figure=fig_as_path_compliance
                     )
                 ]
             )
